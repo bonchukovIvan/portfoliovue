962e2aa0cf2c7950064ebb1ed4e272ea18eab81d
bonchukovIvan <bonchukooov@gmail.com>
2024-05-27 21:53:37 +0300

    first commit

---
+++ resources/js/Components/ApplicationHeader.vue
@@ -0,0 +1,34 @@
+<script>
+
+</script>
+
+<template>
+<header class="pf-header">
+    <div class="container">
+        <div class="pf-header__body">
+            <div class="pf-header__logo">
+                <a href="/">
+                    <h2>Bonchukov Ivan</h2>
+                </a>
+            </div>
+            <nav class="pf-header__menu">
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#">Contacts</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+</template>
+
+<style lang="scss">
+@import '@styles/Components/_header.scss';
+</style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,260 @@
+<script setup>
+import ApplicationHeader from '@/Components/ApplicationHeader.vue';
+import ApplicationFooter from '@/Components/ApplicationFooter.vue';
+
+</script>
+
+<template>
+  <ApplicationHeader />
+  <section class="pf-section pf-about">
+    <div class="container">
+      <div class="pf-about__body">
+        <div class="pf-title pf-section-title">
+          <h2>About</h2>
+        </div>
+        <div class="pf-about__text">
+          <div class="pf-about__v-line"></div>
+          <p>
+            wassup :^)<br>
+            I`m passionate PHP developer with 1 years of experience. Passionate about
+            creating responsive and visually appealing web interfaces. Looking for
+            opportunities to collaborate with skilled teams to deliver exceptional user
+            experiences.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="pf-section pf-skills">
+    <div class="container">
+      <div class="pf-skills__body">
+        <div class="pf-title pf-section-title">
+          <h2>My Skills</h2>
+        </div>
+
+        <div class="pf-skills__cards">
+            <div class="pf-skills__cards-group">
+              <div class="pf-skills__card">
+                <div class="pf-skills__card-body">
+                  <div class="pf-skills__card-img">
+                    <img src="../../../public/php.png" alt="">
+                  </div>
+                  <div class="pf-skills__card-name">
+                    <h5>PHP</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="pf-skills__card">
+                <div class="pf-skills__card-body">
+                  <div class="pf-skills__card-img">
+                    <img src="../../../public/php.png" alt="">
+                  </div>
+                  <div class="pf-skills__card-name">
+                    <h5>PHP</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="pf-skills__card">
+                <div class="pf-skills__card-body">
+                  <div class="pf-skills__card-img">
+                    <img src="../../../public/php.png" alt="">
+                  </div>
+                  <div class="pf-skills__card-name">
+                    <h5>PHP</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="pf-skills__card">
+                <div class="pf-skills__card-body">
+                  <div class="pf-skills__card-img">
+                    <img src="../../../public/php.png" alt="">
+                  </div>
+                  <div class="pf-skills__card-name">
+                    <h5>PHP</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="pf-skills__cards-group">
+
+              <div class="pf-skills__card">
+                <div class="pf-skills__card-body">
+                  <div class="pf-skills__card-img">
+                    <img src="../../../public/php.png" alt="">
+                  </div>
+                <div class="pf-skills__card-name">
+                  <h5>PHP</h5>
+                </div>
+              </div>
+            </div>
+            <div class="pf-skills__card">
+                <div class="pf-skills__card-body">
+                  <div class="pf-skills__card-img">
+                    <img src="../../../public/php.png" alt="">
+                  </div>
+                <div class="pf-skills__card-name">
+                  <h5>PHP</h5>
+                </div>
+              </div>
+            </div>
+        </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <section class="pf-section pf-portfolio">
+    <div class="container">
+      <div class="pf-portfolio__body">
+        <div class="pf-title pf-section-title">
+          <h2>Portfolio</h2>
+        </div>
+        <div class="pf-cards pf-portfolio__cards">
+          <div class="pf-cards-group pf-portfolio__cards-group">
+            <div class="pf-portfolio__card">
+
+              <div class="pf-portfolio__card-body">
+                <div class="pf-portfolio__more">
+                  <div class="pf-portfolio__title">
+                    <h5>Сайт Кафедри військової підготовки СумДу</h5>
+                  </div>
+                  <div class="pf-portfolio__btn">
+                    <button>More information</button>
+                  </div>
+                </div>
+                <div class="pf-portfolio__img">
+                  <img src="../../../public/s.png" alt="">
+                </div>
+              </div>
+              <div class="pf-gradient__gray"></div>
+            </div>
+            <div class="pf-portfolio__card">
+
+<div class="pf-portfolio__card-body">
+  <div class="pf-portfolio__more">
+    <div class="pf-portfolio__title">
+      <h5>Сайт Кафедри військової підготовки СумДу</h5>
+    </div>
+    <div class="pf-portfolio__btn">
+      <button>More information</button>
+    </div>
+  </div>
+  <div class="pf-portfolio__img">
+    <img src="../../../public/s.png" alt="">
+  </div>
+</div>
+<div class="pf-gradient__gray"></div>
+          </div>
+          <div class="pf-portfolio__card">
+
+          <div class="pf-portfolio__card-body">
+            <div class="pf-portfolio__more">
+              <div class="pf-portfolio__title">
+                <h5>Сайт Кафедри військової підготовки СумДу</h5>
+              </div>
+              <div class="pf-portfolio__btn">
+                <button>More information</button>
+              </div>
+            </div>
+            <div class="pf-portfolio__img">
+              <img src="../../../public/s.png" alt="">
+            </div>
+          </div>
+          <div class="pf-gradient__gray"></div>
+          </div>
+          </div>
+          <div class="pf-cards-group pf-portfolio__cards-group">
+            <div class="pf-portfolio__card">
+
+              <div class="pf-portfolio__card-body">
+                <div class="pf-portfolio__more">
+                  <div class="pf-portfolio__title">
+                    <h5>Сайт Кафедри військової підготовки СумДу</h5>
+                  </div>
+                  <div class="pf-portfolio__btn">
+                    <button>More information</button>
+                  </div>
+                </div>
+                <div class="pf-portfolio__img">
+                  <img src="../../../public/s.png" alt="">
+                </div>
+              </div>
+              <div class="pf-gradient__gray"></div>
+            </div>
+            <div class="pf-portfolio__card">
+
+<div class="pf-portfolio__card-body">
+  <div class="pf-portfolio__more">
+    <div class="pf-portfolio__title">
+      <h5>Сайт Кафедри військової підготовки СумДу</h5>
+    </div>
+    <div class="pf-portfolio__btn">
+      <button>More information</button>
+    </div>
+  </div>
+  <div class="pf-portfolio__img">
+    <img src="../../../public/s.png" alt="">
+  </div>
+</div>
+<div class="pf-gradient__gray"></div>
+          </div>
+          <div class="pf-portfolio__card">
+
+          <div class="pf-portfolio__card-body">
+            <div class="pf-portfolio__more">
+              <div class="pf-portfolio__title">
+                <h5>Сайт Кафедри військової підготовки СумДу</h5>
+              </div>
+              <div class="pf-portfolio__btn">
+                <button>More information</button>
+              </div>
+            </div>
+            <div class="pf-portfolio__img">
+              <img src="../../../public/s.png" alt="">
+            </div>
+          </div>
+          <div class="pf-gradient__gray"></div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="pf-section pf-expirience">
+    <div class="container">
+      <div class="pf-expirience__body">
+        <div class="pf-title pf-section-title">
+          <h2>Expirience</h2>
+        </div>
+        <div class="pf-expirience__item">
+          <div class="pf-expirience__item-title">
+            <h3>Center for web-development SumDU</h3>
+          </div>
+          <div class="pf-expirience__project">
+            <div class="pf-expirience__project-item">
+              <h4>Main project:</h4>
+              <p>SumDu Monitoring System, wordpress-based services for checking information about SumDU sites network. Developed from scratch</p>
+            </div>
+            <div class="pf-expirience__project-item">
+              <h4>Work responsibilities:</h4>
+              <p>Developing WordPress and Joomla plugins to gather
+                information about websites. Also, developing a WordPress service that retrieves and
+                manages information from sites, such as details about their latest posts, menu
+                items, etc.
+              </p>
+            </div>
+            <div class="pf-expirience__project-item">
+              <h4>Technologies:</h4>
+              <p>PHP-curl, WordPress, Joomla, ACF, HTML/SCSS/jQuery, Docker+WSL, GIT.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <ApplicationFooter />
+</template>
+
+<style scoped>
+@import '@styles/Components/_header.scss';
+</style>
